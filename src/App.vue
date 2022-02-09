e57cad27b9d58cb071b48a1e71b2c657598751b9
David Huang <mswong@nexify.com.hk>
2022-02-09 17:19:32 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+export default defineComponent({
+  components:{
+    HelloWorld
+  }
+})
+</script>
+
